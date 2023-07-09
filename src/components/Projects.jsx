df8927c07b3d08e80d281cb8d3a6fd6ce35f1e66
Kiipreme <kelvin.a.garcia21@gmail.com>
2023-07-09 18:18:13 -0400

    added a project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import Memory from "../assets/memory.JPG";
 
 const Projects = () => {
   const projects = [
     {
       id: 1,
-      src: "websites here",
+      src: Memory,
+      code: "https://github.com/Kiipreme/Memory-Game",
+      demo: "https://cute-scone-e2d707.netlify.app/",
     },
-    {
-      id: 2,
-      src: "websites here",
-    },
-    {
-      id: 3,
-      src: "websites here",
-    },
+    // {
+    //   id: 2,
+    //   src: "websites here",
+    //   code: "",
+    //   demo: "",
+    // },
   ];
 
   return (
@@ -26,11 +27,10 @@ const Projects = () => {
           <p className="text-4xl font-bold inline border-b-4 border-gray-800">
             Project
           </p>
-          <p className="py-6">Check out some of my work right here</p>
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {projects.map(({ id, src }) => (
+          {projects.map(({ id, src, code, demo }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
@@ -39,10 +39,14 @@ const Projects = () => {
               />
               <div className="flex items-center justify-center">
                 <button className="w-1/2 px-6 py-3 m-4 duation-200 hover:scale-105">
-                  Demo
+                  <a href={demo} target="_blank" rel="noopener noreferrer">
+                    Demo
+                  </a>
                 </button>
                 <button className="w-1/2 px-6 py-3 m-4 duation-200 hover:scale-105">
-                  Code
+                  <a href={code} target="_blank" rel="noopener noreferrer">
+                    Code
+                  </a>
                 </button>
               </div>
             </div>
